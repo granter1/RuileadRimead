f7f2d218d3ae323d3add512cda32758838d0dfd6
granter1 <sean.grant.1@ucdconnect.ie>
2017-04-11 13:34:17 +0100

    Remove Wheel

--- src/UI.java
+++ src/UI.java
@@ -6,17 +6,14 @@ import javax.swing.JPanel;
 
 public class UI extends JFrame {
 	private final JPanel mainPanel;
-	private static Wheel wheel = new Wheel();
 	
 	public UI() {
-		Wheel.loadGame();
 		mainPanel = new JPanel();
-		mainPanel.add(wheel.getWheel());
 		//default size of our window and its layout:
 		setPreferredSize(new Dimension(1000, 1000));
 		getContentPane().setLayout(new GridLayout());
 		getContentPane().add(mainPanel);
-		setTitle("Monopoly");
+		setTitle("Test");
 		pack();
 		setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
